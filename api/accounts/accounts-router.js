8c4c00bd92b4c2d35bca0f1aeb6b4a2d4c2c542f
scoutreilly <scouty33@gmail.com>
2021-12-26 20:38:47 -0700

    catchup

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,69 @@
-const router = require('express').Router()
+const router = require("express").Router();
+const accountModel = require("./accounts-model");
+const middle = require("./accounts-middleware");
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
-
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  accountModel
+    .getAll()
+    .then((accounts) => {
+      res.json(accounts);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
-router.delete('/:id', (req, res, next) => {
+router.get("/:id", middle.checkAccountId, async (req, res) => {
   // DO YOUR MAGIC
-})
+  const account = await accountModel.getById(req.params.id);
+  res.json(account);
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.post(
+  "/",
+  middle.checkAccountPayload,
+  middle.checkAccountNameUnique,
+  async (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      const newAccount = await accountModel.create(req.body);
+      res.status(201).json(newAccount);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.put(
+  "/:id",
+  middle.checkAccountId,
+  middle.checkAccountPayload,
+  async (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      const updated = await accountModel.updateById(req.params.id, req.body);
+      res.json(updated);
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+router.delete("/:id", middle.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    await accountModel.deleteById(req.params.id);
+    res.json(req.account);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.use((err, req, res) => {
+  // eslint-disable-line
+  // DO YOUR MAGIC
+  res.status(err.status || 201).json(err);
+});
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
 const server = express();
-
 server.use(express.json());
+
+const accountsRouter = require("./accounts/accounts-router");
+server.use("/api/accounts/", accountsRouter);
 
 module.exports = server;
